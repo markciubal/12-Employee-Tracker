a4e06e4031f0e7515206b5cd8f20a7b4b95ca5f2
markciubal <mark.ciubal@gmail.com>
2023-06-06 18:23:50 -0700

    Added a few more routes.

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'sqlpassword',
-    database: 'employees_db'
-  },
-  console.log(`Connected to the movies_db database.`)
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'sqlpassword',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the movies_db database.`)
 );
 
 // View all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
@@ -29,7 +29,6 @@ app.get('/api/view-departments', (req, res) => {
             res.json(results)
         }    
     });
-    
 });
 
 // WHEN I choose to view all roles
@@ -43,7 +42,6 @@ app.get('/api/view-roles', (req, res) => {
             res.json(results)
         }    
     });
-    
 });
 
 // WHEN I choose to view all employees
@@ -57,17 +55,47 @@ app.get('/api/view-employees', (req, res) => {
             res.json(results)
         }    
     });
-    
 });
 
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
+app.post('/api/add-department', (req, res) => {
+    console.log(req.body.department);
+    db.query(`INSERT INTO departments (name) VALUES (?);`, req.body.department, (error, results) => {
+        
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.json(results)
+        }    
+    });
+});
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+app.post('/api/add-role', (req, res) => {
+    db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?);`, [req.body.title, req.body.salary, req.body.department_id], (error, results) => {
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.json(results)
+        }    
+    });
+});
 
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+app.post('/api/add-employee', (req, res) => {
+    db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`, [req.body.first_name, req.body.last_name, req.body.role_id, req.body.manager_id], (error, results) => {
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.json(results)
+        }    
+    });
+});
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
